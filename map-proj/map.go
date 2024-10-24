f7d679a393f51f15517878658a24f75c1e7fd7d9
Harshit <prajapati.ha@northeastern.edu>
2024-10-23 20:23:33 -0400

    map commit

--- map-proj/map.go
+++ map-proj/map.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func output(m floatMap) {
+	fmt.Println(m)
+}
+
 func main() {
 	websites := map[string]string{
 		"google": "https://google.com",
@@ -15,4 +21,23 @@ func main() {
 
 	delete(websites, "Google")
 	fmt.Println(websites)
+
+	// creates an array with mentione length
+	userName := make([]string, 2)
+
+	// error prone if use userName := []string{}
+	userName[0] = "Pqr"
+
+	userName = append(userName, "Abc")
+	userName = append(userName, "Xyz")
+
+	// second arg is for length
+	rating := make(floatMap, 3)
+
+	rating["Go"] = 4.5
+	rating["React"] = 4.8
+
+	for i := range userName {
+		print(i)
+	}
 }
